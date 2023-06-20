6e288493a3f7e7e5058ca5ec91fefdc522f679b5
DubrovRuslan <dubrov.ruslan@gmail.com>
2023-06-20 16:12:35 +0300

    Done

---
+++ Helpers/MyCollectionDoubleComparer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Module3HW1.Helpers
+{
+    public class MyCollectionDoubleComparer : IComparer<double>
+    {
+        public int Compare(double x, double y)
+        {
+            if (x < y)
+            {
+                return -1;
+            }
+            else if (x > y)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Helpers/MyCollectionIntComparer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Module3HW1.Helpers
+{
+    public class MyCollectionIntComparer : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x < y)
+            {
+                return -1;
+            }
+            else if (x > y)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
